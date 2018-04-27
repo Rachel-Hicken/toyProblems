3ad6693cf2662981d98b2fb9b83efad44c2ca621
Rachel Hicken <rachkh22@gmail.com>
2018-04-27 12:06:50 -0600

    fail!

---
+++ Cart-part1.js
@@ -0,0 +1,36 @@
+// Over a few toy problems we will be building out a class that constructs a cart object and also includes a few methods to manipulate the data, things like adding to the cart and calculating the total.
+// To start you will want to make a class called Cart that includes a constructor function. Have the constructor function take in a single parameter that will be the items in the cart. Set that value equal to a value called this.cart
+// Second we will want to create a method that will take in an id and then remove the item from the cart with the matching id
+// You will want to save this because we will be adding to it in the future.
+//1. Clarify-why do we set it to this.cart
+//2. matchID var, array to hold cart items
+//3/4 use provided data
+//5. constructor(items){super()???}
+//6. methods: remove an item filter, findIndex
+//7. create a class
+    //constructor
+
+class Cart{
+    constructor(items){
+
+    }
+}
+
+let sampleCartData=[{
+  id: 1,
+  product: 'Bob Ross Paint Kit',
+  price: 45.99,
+  quantity: 2
+}, {
+  id: 2,
+  product: 'Paint Palette',
+  price: 7.99,
+  quantity: 3
+}, {
+  id: 3,
+  product: 'Paint Thinner',
+  price: 15.99,
+  quantity: 2
+}];
+
+
